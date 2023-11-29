ee2a1dba366ad2ba7b9d48af95e5ee69b769543b
Vinayak-kVA <142249608+Vinayak-kVA@users.noreply.github.com>
2023-11-29 22:41:04 +0530

    jkffstr_tb.v

--- 3. JK FF/JK Flipflop Structural Level/jkffstr_tb.v
+++ 3. JK FF/JK Flipflop Structural Level/jkffstr_tb.v
@@ -1,36 +1,38 @@
-`timescale 1ns/1ns // Set timescale for simulation
-`include "jkffstr.v" //Including code file
+`timescale 1ns / 1ps
+module fifo_tb();
+reg J,K,clk;
+wire Q, Qbar;
 
-module jkffstr_tb;
-reg J,K, CLK;
-wire Q1,QBAR1,Q2,QBAR2;
-integer i;
-//input output declaration
-jkffstr uut (.q1(Q1), .qbar1(QBAR1), .q2(Q2), .qbar2(QBAR2), .clk(CLK), .j(J), .k(K)); 
+jkff uut (J,K,Q,Qbar,clk);
 
-initial begin
-  CLK=0; //forcing outputs to predefined conditions
-  force Q1 =1;
-  force QBAR1=0;
-  force Q2 =0;
-  force QBAR2=1;
-  #1  //releasing outputs
-  release Q1;
-  release Q2;
-  release QBAR1;
-  release QBAR2;
-//generating clock
-     for(i=0;i<10;i=i+1)
-          #10 CLK = ~CLK;  
-end 
-initial begin 
-    $dumpfile("jkffstr.vcd");  //generating vcd file
-    $dumpvars(0, jkffstr_tb);
- $monitor("  J = %b K = %b Q= %b QBAR = %b clk=\t",J,K,Q2,QBAR2,CLK);
- //testcases
- J= 1; K= 0;
- #20; J= 0; K= 1; 
- #20; J= 0; K= 0; 
- #20; J= 1; K=1; 
-end 
+initial
+begin
+clk = 1'b0;
+end
+
+always #5 clk = ~ clk;
+
+initial 
+begin
+force Q=1;
+force Qbar=0;
+force jkff.w3=1;
+force jkff.w4=0;
+#1
+release Q;
+release Qbar;
+release jkff.w3;
+release jkff.w4;
+end
+
+initial
+begin
+J=0; K=0;
+#7 J=1; K=0;
+#10 J=1; K=1;
+#10 J=1; K=1;
+#15 J=0; K=0;
+#100 $finish;
+end
+
 endmodule
